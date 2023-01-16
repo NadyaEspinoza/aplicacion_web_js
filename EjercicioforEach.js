e639e607894bc23b3ee527b4bdd05a26e5f47cd4
NadyaEspinoza <nadyaespinoza1698@gmail.com>
2023-01-16 08:04:31 -0700

    Ejercicio API

---
+++ EjercicioforEach.js
@@ -0,0 +1,21 @@
+//const edades= [22,4,5,8,9];
+//edades.forEach((valor) => {
+//console.log(valor);
+//});
+
+const usuarios = [
+    {
+        nombre: 'Camilo',
+        especie:'gato',
+        genero: 'Macho'
+    },
+    {
+        nombre: 'Piter',
+        especie:'perro',
+        genero: 'Macho'
+    }
+];
+usuarios.forEach((usuario) => {
+    console.log(usuario.nombre);
+});
+
